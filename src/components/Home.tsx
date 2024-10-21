94901b74266fbcb884623d08ec1855263e9b90b2
Daniel Muiruri <gathigidg26@gmail.com>
2024-10-21 13:17:21 +0300

    :sparkles:Adding animations on the homepage of the website

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,9 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import NavBar from './Navbar'
 import Footer from './Footer'
 import { useSwipeable } from "react-swipeable";
 import { Link, NavLink } from 'react-router-dom';
+// import { ReactTyped } from 'react-typed';
 
 
 const Home = () => {
@@ -38,48 +39,86 @@ const Home = () => {
         preventScrollOnSwipe: true,
         trackMouse: true,
       });
+
+      const [visible, setVisible] = useState(false);
+
+      useEffect(() => {
+        const timer = setTimeout(() => {
+          setVisible(true);
+        }, 1000);
+    
+        return () => clearTimeout(timer); 
+      }, []);
+      
+
   return (
     <>
     <NavBar />
     <div>
         {/* 0 */}
-        <div className='h-auto bg-gray-400 text-black py-24 text-center md:text-5xl sm:text-3xl text-3xl'>
-        <p>Welcome to the official website of <b>Morgan Technical Training.</b></p>
-        </div>
+
         {/* part 1 */}
-        <section className="relative bg-gray-800 h-[700px]">
-  <div className="absolute inset-0">
-    <img
-      src="https://miro.medium.com/v2/resize:fit:1400/0*7VyEZgzwUhQMeBqb"
-      alt="Hero Background"
-      className="object-cover w-full h-[700px]"
-    />
-    <div className="absolute inset-0 bg-black opacity-50"></div> {/* Dark overlay for readability */}
-  </div>
-  <div className="container mx-auto flex flex-col items-center justify-center h-full text-center text-white relative z-10 px-4">
-    <h1 className="text-5xl md:text-6xl font-bold mb-4">Empowering Your Digital Future</h1>
-    <p className="text-lg md:text-xl mb-8">
-      Join us in navigating the world of technology with innovative solutions designed for your success.
-    </p>
-    <Link to={'/courses'}><p className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition cursor-pointer">
-      Get Started
-    </p> </Link>
-  </div>
-</section>
+        <section className="relative bg-gray-800 h-[700px] flex flex-col justify-center">
+          <div className="absolute inset-0">
+            <img src="https://miro.medium.com/v2/resize:fit:1400/0*7VyEZgzwUhQMeBqb" alt="Hero Background" className={`object-cover w-full h-full transition-opacity duration-1000 ease-in ${visible ? 'opacity-100' : 'opacity-0'}`} />
+            <div className="absolute inset-0 bg-black opacity-60"></div> 
+          </div>
+
+          <div className="container mx-auto flex flex-col items-center justify-center h-full text-center text-white relative z-10 px-4">
+            <h1 className={`text-4xl md:text-5xl font-bold text-white transition-opacity duration-1000 ease-in ${visible ? 'opacity-100' : 'opacity-0'}`}>
+              Welcome to <span className="text-blue-500">Morgan Technical Training</span>
+            </h1>
+
+            <h2 className={`text-3xl md:text-3xl font-bold mb-4 transition-opacity duration-1000 ease-in ${visible ? 'opacity-100' : 'opacity-0'} delay-1000`}>
+              Empowering Your Digital Future
+            </h2>
+
+            <p className={`text-md md:text-lg mb-8 transition-opacity duration-1000 ease-in ${visible ? 'opacity-100' : 'opacity-0'} delay-1600`}>
+              Join us in navigating the world of technology with innovative solutions designed for your success.
+            </p>
+
+            {/* <div className="text-lg md:text-xl font-bold text-blue-500 md:pl-4 pl-2 text-shadow mb-4">
+              <ReactTyped 
+                strings={['Frontend Programming', 'Graphics Design', 'Cyber Security', 'Android Programming']} 
+                typeSpeed={120} 
+                backSpeed={140} 
+                loop 
+              />
+            </div> */}
+
+            <Link to="/courses">
+              <p className={`bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 cursor-pointer transition-opacity duration-1000 ease-in ${visible ? 'opacity-100' : 'opacity-0'} delay-1600`}>
+                Get Started
+              </p>
+            </Link>
+          </div>
+        </section>
+
+
 
     {/* part 2 */}
     <div className="bg-gray-400 py-16">
       <div className="container mx-auto px-4">
         <div className="flex flex-col lg:flex-row items-center">
           {/* Left Column */}
-          <div className="w-full lg:w-1/2 mb-8 lg:mb-0 lg:pr-8">
-            <h2 className="text-3xl lg:text-5xl font-bold mb-4">Committed to Excellence</h2>
+          <div
+            className={`w-full lg:w-1/2 mb-8 lg:mb-0 lg:pr-8 transition-opacity duration-2000 ease-in-out ${
+              visible ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            <h2 className="text-3xl lg:text-5xl font-bold mb-4">
+              Committed to Excellence
+            </h2>
             <p className="text-lg lg:text-xl mb-8">
-              Our mission is to provide outstanding tech skills to our students and ensure they feel satisfied.We have a variety of fascinating courses ready for learning now at affordable prices.
-              Remember, The secret of getting ahead is getting started.
+              Our mission is to provide outstanding tech skills to our students
+              and ensure they feel satisfied. We have a variety of fascinating
+              courses ready for learning now at affordable prices. Remember, the
+              secret of getting ahead is getting started.
             </p>
-            <h3 className="text-2xl font-bold mb-4">Hear Our students</h3>
-            <p className="text-lg lg:text-xl mb-8">“The digital world is here and we must embrace it.”</p>
+            <h3 className="text-2xl font-bold mb-4">Hear Our Students</h3>
+            <p className="text-lg lg:text-xl mb-8">
+              “The digital world is here, and we must embrace it.”
+            </p>
           </div>
 
           {/* Right Column: Slider */}
@@ -87,13 +126,15 @@ const Home = () => {
             <div className="relative" {...handlers}>
               <div className="overflow-hidden relative h-64">
                 <div
-                  className="flex transition-transform duration-500"
+                  className={`flex duration-2000 transition-opacity duration-1000 ease-in ${
+            visible ? 'opacity-100' : 'opacity-0'
+          } delay-1000`}
                   style={{ transform: `translateX(-${currentIndex * 100}%)` }}
                 >
                   {testimonials.map((testimonial, index) => (
                     <div
                       key={index}
-                      className="flex-shrink-0 w-full h-full flex items-center justify-center bg-white text-black shadow-lg p-6"
+                      className="flex-shrink-0 w-full h-full flex items-center justify-center bg-white text-black shadow-lg p-6 transition-transform duration-2000 ease-in-out"
                     >
                       <div className="text-center">
                         <p className="text-lg mb-4">{testimonial.testimonial}</p>
@@ -118,7 +159,9 @@ const Home = () => {
                   <div
                     key={index}
                     className={`w-2.5 h-2.5 mx-1 rounded-full cursor-pointer ${
-                      index === currentIndex ? 'bg-blue-500' : 'bg-gray-300'
+                      index === currentIndex
+                        ? 'bg-blue-500'
+                        : 'bg-gray-300'
                     }`}
                     onClick={() => goToSlide(index)}
                   ></div>
@@ -132,8 +175,12 @@ const Home = () => {
     {/* part 3 */}
     <section className="bg-gray-100 py-16 relative">
   <div className="container mx-auto px-4">
-    <h2 className="text-4xl font-bold text-center mb-10 underline">Top Courses</h2>
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <h2 className={`text-4xl font-bold text-center mb-10 underline duration-1000 ease-in ${
+            visible ? 'opacity-100' : 'opacity-0'
+          } delay-1600`}>Top Courses</h2>
+    <div className={`grid grid-cols-1 md:grid-cols-3 gap-8 transition-opacity duration-1000 ease-in ${
+            visible ? 'opacity-100' : 'opacity-0'
+          } delay-1600`}>
       {/* Feature 1 */}
       <div className="bg-white p-6 rounded-lg shadow-lg text-center">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHOidFsuemGaWGxr16QKfvzI52EFQeFTrN4Q&s" alt="Feature 1" className="mx-auto mb-4 h-16" /> {/* Replace with your icon */}
@@ -184,7 +231,9 @@ const Home = () => {
 </section>
 
 {/* part 4 */}
-<section className="py-12 bg-gray-200">
+<section className={`py-12 bg-gray-200 duration-1000 ease-in ${
+            visible ? 'opacity-100' : 'opacity-0'
+          } delay-1600`}>
       <div className="container mx-auto px-4">
         {/* Heading */}
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">Embrace the digital world</h2>
